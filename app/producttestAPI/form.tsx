82ef4f2fed5d54fa038bd21d02a0c98df595783b
kritapas wongpemdacha <pame7981@gmail.com>
2024-03-01 14:38:34 +0700

    upload pdf and csv product file

--- app/producttestAPI/form.tsx
+++ app/producttestAPI/form.tsx
@@ -1,43 +1,58 @@
 'use client'
 
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import { useEffect, useState } from "react"
+import * as XLSX from 'xlsx';
+import ReactPDF from '@react-pdf/renderer';
+import { MyDocument } from './pdf/report1'
 
-export default function Form({items}: any)
-{
+
+export default function Form({ items }: any) {
     const [productState, setProductState] = useState([])
     const [pageState, setPageState] = useState({
         page: 1
     })
     const [search, SetSearch] = useState('')
-    useEffect(()=>{
+    const [isClient, setIsClient] = useState(false)
+    useEffect(() => {
         setProductState(items.data)
+        setIsClient(true)
     }, [])
 
-    const handelOnClick = async () =>{
-        const page : number = pageState.page + 1
+    const handelOnClick = async () => {
+        const page: number = pageState.page + 1
         const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/producttest?search=${search}&page=${page}`)
         const result = await response.json()
         setProductState(result.data)
-        setPageState({page})
+        setPageState({ page })
     }
-    const handelOnBack = async () =>{
-        const page : number = pageState.page - 1
+    const handelOnBack = async () => {
+        const page: number = pageState.page - 1
         const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/producttest?search=${search}&page=${page}`)
         const result = await response.json()
         setProductState(result.data)
-        setPageState({page})
+        setPageState({ page })
     }
 
     const searchInput = async (event) => {
         const searchstring = event.target.value
-        const page : number = 1
+        const page: number = 1
         const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/producttest?search=${searchstring}&page=${page}`)
         const result = await response.json()
         setProductState(result.data)
-        setPageState({page})
+        setPageState({ page })
         SetSearch(searchstring)
     }
-    console.log(items)
+    //export data csv
+    const exportCsv = () => {
+        const worksheet = XLSX.utils.json_to_sheet(productState);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+        //let buffer = XLSX.write(workbook, { bookType: "xlsx", type: "buffer" });
+        //XLSX.write(workbook, { bookType: "xlsx", type: "binary" });
+        XLSX.writeFile(workbook, "DataSheet.xlsx");
+    }
+
     return (
         <div>
             <div>
@@ -45,13 +60,24 @@ export default function Form({items}: any)
             </div>
             <div>
                 {
-                    productState.map((item:any)=>{
+                    productState.map((item: any) => {
                         return <div key={item.id}><div>{item.name}</div><div className="border">{item.pathFileName}</div></div>
                     })
                 }
             </div>
-           <button className="btn btn-success" onClick={handelOnBack} >Click Back</button>
-           <button className="btn btn-success" onClick={handelOnClick} >Click Next</button>
+            <button className="btn btn-success" onClick={handelOnBack} >Click Back</button>
+            <button className="btn btn-success" onClick={handelOnClick} >Click Next</button>
+            <div>
+                <button className="btn btn-warning" onClick={exportCsv} >Export CSV</button>
+                {
+                    isClient ?
+                
+                    <PDFDownloadLink document={<MyDocument data={productState} />} fileName="somename.pdf" className="btn btn-secondary">
+                        Download now!
+                    </PDFDownloadLink>
+                    : null
+                }
+            </div>
         </div>
     )
 }
